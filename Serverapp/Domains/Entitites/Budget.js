b62e44f92988cc1a8cad8f626813a39722e857af
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-03 13:27:34 +0700

    feat(budget&package): add budget entities and attribute

---
+++ Serverapp/Domains/Entitites/Budget.js
@@ -0,0 +1,59 @@
+import { Sequelize } from "sequelize";
+import db from "../../Configuration/Database.js";
+import { v4 as uuidv4 } from "uuid";
+
+const { DataTypes } = Sequelize;
+
+const budget = db.define("tb_m_budgets", {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    defaultValue: uuidv4(),
+    allowNull: false,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [5, 150],
+    },
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [5, 350],
+    },
+  },
+  created_date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+  },
+  created_by: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+  },
+  updated_by: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  deleted_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  delete_by: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  is_deleted: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+});
