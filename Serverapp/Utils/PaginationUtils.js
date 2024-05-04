a0e9631702874a764b323327b91c196e098319c6
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-04 12:49:43 +0700

    feat(Utils): add pagination configuration

---
+++ Serverapp/Utils/PaginationUtils.js
@@ -0,0 +1,19 @@
+function getPagination(page, pageSize) {
+  const limit = pageSize ? +pageSize : 10;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+}
+
+function getPagingData(data, page, limit) {
+  const { count: totalItem, rows: items } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalItem / limit);
+
+  return { totalItem, items, totalPages, currentPage };
+}
+
+module.exports = {
+  getPagination,
+  getPagingData,
+};
