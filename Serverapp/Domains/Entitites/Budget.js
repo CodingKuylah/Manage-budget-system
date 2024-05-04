8908263eb917911044586a59af631c7819118e1a
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-04 12:06:52 +0700

    fix(Budget): add async await for budget model

--- Serverapp/Domains/Entitites/Budget.js
+++ Serverapp/Domains/Entitites/Budget.js
@@ -4,56 +4,73 @@ import { v4 as uuidv4 } from "uuid";
 
 const { DataTypes } = Sequelize;
 
-const budget = db.define("tb_m_budgets", {
-  id: {
-    type: DataTypes.STRING,
-    primaryKey: true,
-    defaultValue: uuidv4(),
-    allowNull: false,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [5, 150],
+const Budget = db.define(
+  "tb_m_budgets",
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      defaultValue: uuidv4(),
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5, 150],
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5, 350],
+      },
+    },
+    created_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    created_by: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updated_by: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    delete_by: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    is_deleted: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
     },
   },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [5, 350],
-    },
-  },
-  created_date: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-  },
-  created_by: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-  },
-  updated_by: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  deleted_at: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-  delete_by: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  is_deleted: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0,
-  },
-});
+  {
+    freezeTableName: true,
+  }
+);
+
+export default Budget;
+
+(async () => {
+  try {
+    await db.sync();
+    console.log("db connecteed");
+  } catch (error) {
+    console.error("error syncing budget database" + error);
+  }
+})();
