4b4cb3d9b016f1f33765a9087cd02531d907a390
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-08 12:48:11 +0700

    feat(BudgetRequest): add budget DTO for create budget

---
+++ Serverapp/Domains/Models/Requests/BudgetRequest.js
@@ -0,0 +1,28 @@
+class BudgetRequest {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+  }
+  validate() {
+    const errors = [];
+    if (!this.title) {
+      errors.push("Title must be filled !");
+    }
+    if (this.title.length < 5) {
+      errors.push("Title length cannot under 5 words !");
+    }
+    if (this.title.length > 150) {
+      errors.push("Title length is 150 max words");
+    }
+    if (this.description.length < 5) {
+      errors.push("Title length cannot under 5 words !");
+    }
+    if (this.description.length > 150) {
+      errors.push("Title description is 350 max words");
+    }
+
+    return errors;
+  }
+}
+
+export default BudgetRequest;
