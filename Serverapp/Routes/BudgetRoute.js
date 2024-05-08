d17df80bb4ee40513d1e66fa83fcb3e288104f19
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-08 13:08:42 +0700

    feat(Budget): add create budget on budget routes

--- Serverapp/Routes/BudgetRoute.js
+++ Serverapp/Routes/BudgetRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getById, getAllBudget } from "../Services/BudgetService.js";
+import {
+  getById,
+  getAllBudget,
+  createBudget,
+} from "../Services/BudgetService.js";
 
 const router = express.Router();
 
 router.get("budget/:id", getById);
 router.get("budget", getAllBudget);
+router.post("budget", createBudget);
 
 export default router;
