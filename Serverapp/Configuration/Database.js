3c4cd3b3ca5d8327a3fa6dae029e596ea584f60c
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-09 12:28:38 +0700

    fix(database): change logging and add timezone

--- Serverapp/Configuration/Database.js
+++ Serverapp/Configuration/Database.js
@@ -3,7 +3,8 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize("db_MANAGEMENT_BUDGET_SYSTEM", "root", "", {
   host: "localhost",
   dialect: "mysql",
-  logging: console.log(),
+  logging: console.log,
+  timezone: "+07:00",
 });
 
 export default db;
