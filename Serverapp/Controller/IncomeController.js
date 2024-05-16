9c2e6df6a7ced64fcf5d1da1cd1480ddddec15ce
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-16 08:18:31 +0700

    feat(Income): add function to soft delete specific income

--- Serverapp/Controller/IncomeController.js
+++ Serverapp/Controller/IncomeController.js
@@ -35,4 +35,30 @@ async function getAllIncome(req, res) {
   }
 }
 
-export { getIncomeById, getAllIncome };
+async function deleteIncome(req, res) {
+  try {
+    const id = req.params.id;
+    const income = await Income.findOne({
+      where: {
+        id: id,
+        is_deleted: 0,
+      },
+    });
+    if (income) {
+      income.is_deleted = 1;
+      await income.save();
+      handleResponse(
+        res,
+        income,
+        200,
+        "Income with id : " + id + " is successfully deleted"
+      );
+    } else {
+      res.status(404).json({ error: "Income with id " + id + " is not found" });
+    }
+  } catch (error) {
+    handleError(res, error);
+  }
+}
+
+export { getIncomeById, getAllIncome, deleteIncome };
