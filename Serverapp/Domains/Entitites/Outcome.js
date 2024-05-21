93ab136f47bf7fdf1ca327bfe6b15a3a38072e5b
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-21 12:32:43 +0700

    feat(Outcome): add outcome entity

---
+++ Serverapp/Domains/Entitites/Outcome.js
@@ -0,0 +1,74 @@
+import { Sequelize } from "sequelize";
+import db from "../../Configuration/Database.js";
+import { v4 as uuid4 } from "uuid";
+
+const { DataTypes } = Sequelize;
+
+const Outcome = db.define(
+  "tb_m_outcome",
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      defaultValue: uuid4(),
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5, 150],
+      },
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5, 350],
+      },
+    },
+    created_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    created_by: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+    updated_by: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    delete_by: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    is_deleted: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+export default Outcome(async () => {
+  try {
+    await db.sync();
+  } catch (error) {
+    console.error("Error syncing Outcome database " + error);
+  }
+});
