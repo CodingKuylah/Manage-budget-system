9606562c90adac270994a9aa8ca057eee9eb2792
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-21 13:41:22 +0700

    feat(App): add route for app.js

--- Serverapp/app.js
+++ Serverapp/app.js
@@ -3,11 +3,12 @@ import cors from "cors";
 
 import BudgetRoute from "./Routes/BudgetRoute.js";
 import IncomeRoute from "./Routes/IncomeRoute.js";
+import OutcomeRoute from "./Routes/OutcomeRoute.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(BudgetRoute, IncomeRoute);
+app.use(BudgetRoute, IncomeRoute, OutcomeRoute);
 
 app.listen("5000", () => {
   console.log(
