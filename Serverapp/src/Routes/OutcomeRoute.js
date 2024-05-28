7a970ffdd8c3f3f4c996e480d8dd003adf0c1760
fajar <fajar.anwarimaulana99@gmail.com>
2024-05-28 12:39:49 +0700

    Feat(Outcome): add mapping route for minus value budget function

--- Serverapp/src/Routes/OutcomeRoute.js
+++ Serverapp/src/Routes/OutcomeRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getAllOutcome,
   getOutcomeById,
+  minusOutcomeValue,
   deleteOutcome,
 } from "../Controller/OutcomeController.js";
 import { baseRoute } from "./Base/BaseRoute.js";
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.get(baseRoute + "outcome/:id", getOutcomeById);
 router.get(baseRoute + "admin/outcome", getAllOutcome);
+router.post(baseRoute + "outcome/minus-budget/:outcomeId", minusOutcomeValue);
 router.delete(baseRoute + "outcome/:id", deleteOutcome);
 
 export default router;
