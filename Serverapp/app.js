28bbf6031f408722cfc311144ac3d4785d52f090
fajar <fajar.anwarimaulana99@gmail.com>
2024-06-06 13:32:04 +0700

    Feat(App): add auth route

--- Serverapp/app.js
+++ Serverapp/app.js
@@ -5,11 +5,17 @@ import BudgetRoute from "./src/Routes/BudgetRoute.js";
 import IncomeRoute from "./src/Routes/IncomeRoute.js";
 import OutcomeRoute from "./src/Routes/OutcomeRoute.js";
 import HistoriesRoute from "./src/Routes/HistoriesRoute.js";
+import AuthRoute from "./src/Routes/AuthRoute.js";
+import Account from "./src/Domains/Entitites/Account.js";
+import User from "./src/Domains/Entitites/User.js";
+
+// User.hasOne(Account, { foreignKey: "user_id", as: "account" });
+// Account.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(BudgetRoute, IncomeRoute, OutcomeRoute, HistoriesRoute);
+app.use(BudgetRoute, IncomeRoute, OutcomeRoute, HistoriesRoute, AuthRoute);
 
 app.listen("5000", () => {
   console.log(
