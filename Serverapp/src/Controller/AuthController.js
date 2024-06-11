0b1cc1698cc3af12727ba6bbf52d3198ecfc7e73
fajar <fajar.anwarimaulana99@gmail.com>
2024-06-11 13:45:00 +0700

    Fix(Auth): add email validator and register function

--- Serverapp/src/Controller/AuthController.js
+++ Serverapp/src/Controller/AuthController.js
@@ -5,19 +5,40 @@ import Account from "../Domains/Entitites/Account.js";
 import User from "../Domains/Entitites/User.js";
 import bcryptjs from "bcryptjs";
 
+function emailValidator(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.(com|co\.id)$/i;
+  return emailPattern.test(email);
+}
+
 async function register(req, res) {
   try {
-    const { username, password, email, numberPhone, gender } = req.body;
-    const hashPassword = await bcryptjs.hash(password, 10);
-    const newUser = await User.create({ name, email, numberPhone, gender });
-    const newAccount = await Account.create({
-      username,
-      hashPassword,
-      userId: newUser.id,
+    const { username, password, email, number_phone, gender } = req.body;
+
+    if (!emailValidator(email)) {
+      return handleResponse(
+        res,
+        email,
+        400,
+        "Invalid email format. Email must contain '@' and a valid domain like '.com' or '.co.id'"
+      );
+    }
+    const salt = await bcryptjs.genSalt();
+    const hashPassword = await bcryptjs.hash(password, salt);
+    const newUser = await User.create({
+      name: username,
+      email: email,
+      number_phone: number_phone,
+      gender: gender,
     });
 
-    const handlingAccount = new RegisterResponse(newAccount.username, password);
-    handleResponse(res, handlingAccount, 200, "User is successfully registred");
+    await Account.create({
+      id: newUser.id,
+      username: username,
+      password: hashPassword,
+    });
+
+    const handlingResponse = new RegisterResponse(username, password);
+    handleResponse(res, handlingResponse, 200, "Register is successfully");
   } catch (error) {
     handleError(res, error);
   }
