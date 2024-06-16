cd69506fb904ed3b73f8c35d69fd09ff2523307d
fajar <fajar.anwarimaulana99@gmail.com>
2024-06-16 13:20:15 +0700

    Fix(DTO): change register response status

--- Serverapp/src/Domains/Models/Responses/RegisterResponse.js
+++ Serverapp/src/Domains/Models/Responses/RegisterResponse.js
@@ -1,7 +1,9 @@
 class RegisterResponse {
-  constructor(username, password) {
+  constructor(username, password, verification_code, account_status) {
     this.username = username;
     this.password = password;
+    this.verification_code = verification_code;
+    this.account_status = account_status;
   }
 }
 
