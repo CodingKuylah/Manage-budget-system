29e5b350d0c81d37cfd0181ef015c3f1d94853ee
fajar <fajar.anwarimaulana99@gmail.com>
2024-06-21 13:31:17 +0700

    Feat(UserOutcome): add relational between user and outcome

---
+++ Serverapp/src/Domains/Entitites/TransactionalTable/UserOutcome.js
@@ -0,0 +1,36 @@
+import { Sequelize } from "sequelize";
+import db from "../../../Configuration/Database.js";
+import { v4 as uuidV4 } from "uuid";
+
+const { DataTypes } = Sequelize;
+
+const UserOutcome = db.define(
+  "tb_tr_user_outcome",
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      defaultValue: () => uuidV4(),
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    outcomeId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+
+export default UserOutcome;
