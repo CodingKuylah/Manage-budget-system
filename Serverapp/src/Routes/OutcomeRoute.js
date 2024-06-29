6c946feed76767f8ef25ffef518261f6e050ddf0
fajar <fajar.anwarimaulana99@gmail.com>
2024-06-29 10:05:45 +0700

    Feat(Outcome): add verify token

--- Serverapp/src/Routes/OutcomeRoute.js
+++ Serverapp/src/Routes/OutcomeRoute.js
@@ -5,13 +5,19 @@ import {
   minusOutcomeValue,
   deleteOutcome,
 } from "../Controller/OutcomeController.js";
+
 import { baseRoute } from "./Base/BaseRoute.js";
+import { verifyToken } from "../Controller/AuthController.js";
 
 const router = express.Router();
 
 router.get(baseRoute + "outcome/:id", getOutcomeById);
 router.get(baseRoute + "admin/outcome", getAllOutcome);
-router.put(baseRoute + "outcome/minus-budget/:outcomeId", minusOutcomeValue);
+router.put(
+  baseRoute + "outcome/minus-budget/:outcomeId",
+  verifyToken,
+  minusOutcomeValue
+);
 router.delete(baseRoute + "outcome/:id", deleteOutcome);
 
 export default router;
