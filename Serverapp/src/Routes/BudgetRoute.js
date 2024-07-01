d9b956f2d50dfa434ac2a86285ee03982cd2cb7e
fajar <fajar.anwarimaulana99@gmail.com>
2024-07-01 10:12:22 +0700

    Feat(Budget): add connect user and budget route

--- Serverapp/src/Routes/BudgetRoute.js
+++ Serverapp/src/Routes/BudgetRoute.js
@@ -2,15 +2,18 @@ import express from "express";
 import {
   getById,
   getAllBudget,
+  connectBudgetAndUser,
   createSingleBudget,
   deleteBudget,
 } from "../Controller/BudgetController.js";
 import { baseRoute } from "./Base/BaseRoute.js";
+import { verifyToken } from "../Controller/AuthController.js";
 
 const router = express.Router();
 
 router.get(baseRoute + "budget/:id", getById);
 router.get(baseRoute + "admin/budget", getAllBudget);
+router.post(baseRoute + "budget/connect", verifyToken, connectBudgetAndUser);
 router.post(baseRoute + "budget/insert", createSingleBudget);
 router.delete(baseRoute + "admin/budget/:id", deleteBudget);
 
